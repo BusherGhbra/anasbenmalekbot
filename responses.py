992f490ddea8dbbda33e03fc30bc5ad7db43fe51
Bsher Ghbra <34723373+BusherGhbra@users.noreply.github.com>
2021-06-02 19:49:52 +0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,26 +1,27 @@
-from http import server
 import io
-from pickle import load
 import gspread
 import constants
-import pandas as pd
 from Google import Create_Service
 from googleapiclient.http import MediaIoBaseDownload
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-global points, students, service, pdfs
+sheets = {}
+global service
 
 
 def load_data():
-    global points, students, service, pdfs
-
     creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', constants.SCOPES)
     client = gspread.authorize(creds)
 
-    points = client.open('points').sheet1
-    students = client.open('students').sheet1
+    sheets['points'] = client.open('points').sheet1
+    sheets['students'] = client.open('students').sheet1
+    sheets['P'] = client.open('pages').sheet1
+    sheets['S'] = client.open('sections').sheet1
+    sheets['N'] = client.open('notes').sheet1
+    sheets['A'] = client.open('attendance').sheet1
 
+    global service
     service = Create_Service(
         constants.CLIENT_SECRET_FILE,
         constants.API_NAME, 
@@ -28,30 +29,26 @@ def load_data():
         constants.SCOPES
     )
 
-    query = f"parents = '{constants.DRIVE_FOLDER}'"
-    result = service.files().list(q=query).execute()
-    loaded_files = result.get('files')
-    nextPageToken = result.get('nextPageToken')
 
-    while nextPageToken:
-        result = service.files().list(q=query).execute()
-        loaded_files.extend(result.get('files'))
-        nextPageToken = result.get('nextPageToken')
-
-    pdfs = pd.DataFrame(loaded_files)
+def get_row(username):
+    try:
+        row = int(username[1:])
+        if row > sheets['students'].row_count:
+            raise Exception
+    except Exception:
+        return 0
+    return row
 
 
 def validate_user(username, password):
-    try:
-        row = 2 if username.lower() in constants.ADMIN else int(username[1:]) + constants.SHEET_OFFSET
-        if row > students.row_count:
-            raise Exception
-    except Exception:
-        return ''
+    if (username, password) in constants.ADMIN:
+        return username
 
-    data = students.row_values(row)
-    if data[0].lower() == username.lower() and data[1].lower() == password.lower():
-        return data[2]
+    row = get_row(username)
+    if row:
+        data = sheets['students'].row_values(row)
+        if data[0].lower() == username.lower() and data[1].lower() == password.lower():
+            return data[2]
     return ''
 
 
@@ -71,30 +68,33 @@ def get_keyboard(context):
 
 
 def get_points(username):
-    try:
-        row = 2 if username.lower() in constants.ADMIN else int(username[1:]) + constants.SHEET_OFFSET
-    except Exception:
-        return 0
-
-    data = points.row_values(row)
-    if data[0].lower() == username.lower():
-        return int(data[1])
+    row = get_row(username)
+    if row:
+        data = sheets['points'].row_values(row)
+        if data[0].lower() == username.lower():
+            return int(data[1])
     return 0
 
 
 def get_file(username, file_type):
     fh = io.BytesIO()
-    for file_name, file_id in zip(pdfs['name'], pdfs['id']):
-        if str(file_name).lower() == f'{username}_{file_type}.pdf'.lower():
-            request = service.files().get_media(fileId=file_id)
-            downloader = MediaIoBaseDownload(fd=fh, request=request)
+    row = get_row(username)
+    sheet = sheets.get(file_type, None)
 
-            done = False
-            while not done:
-                _, done = downloader.next_chunk()
+    if not row or not sheet:
+        return False, fh
 
-            fh.seek(0)
-            fh.name = file_name
-            return True, fh
+    data = sheet.row_values(row)
+    if username.lower() == data[0].lower():
+        request = service.files().get_media(fileId=data[1])
+        downloader = MediaIoBaseDownload(fd=fh, request=request)
+
+        done = False
+        while not done:
+            _, done = downloader.next_chunk()
+
+        fh.seek(0)
+        fh.name = f'{username.upper()}_{file_type}.pdf'
+        return True, fh
 
     return False, fh
